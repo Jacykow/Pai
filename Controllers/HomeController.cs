f862cc754a442f246b2259e4ab2da9046b9bf617
Jacykow <jacgulij@gmail.com>
2020-06-08 14:01:56 +0200

    views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Pai.Models;
+using System.Diagnostics;
 
 namespace Pai.Controllers
 {
@@ -23,7 +19,17 @@ namespace Pai.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Tournaments()
         {
             return View();
         }
